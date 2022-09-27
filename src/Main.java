94e21e18f5e808621e9ebd774686062883a698c3
christianborn <chrisborn@hotmail.de>
2022-09-27 12:17:17 +0200

    Added Hello Mykola

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bye World, bye everybody!");
-
+        System.out.println("Hi Mykola!");
 
         }
     }
