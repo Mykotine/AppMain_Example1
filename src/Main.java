ea4cdddd8b3cc9cc3574e3b44233eb4a141b37bb
mykola <bobirets@gmail.com>
2022-10-06 14:35:05 +0200

    rebased to master

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bye World, bye everybody!");
-        System.out.println("Hi Mykola!");
+        System.out.println("Mykola war hier");
+
 
         }
     }
