e9ca56173a84159e62b021deec2fc97fd0e0fd1e
mykola <bobirets@gmail.com>
2022-10-06 16:19:16 +0200

    add feature

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bye World, bye everybody!");
         System.out.println("Mykola war hier");
+        System.out.println("Feature");
 
 
         }
